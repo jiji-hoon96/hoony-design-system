6ff0f983a0912a54c3527b85e0899ae36c83d2eb
JIHOON LEE <jihoon.lee@hicare.net>
2025-06-01 12:08:23 +0900

    fix : prettier

--- apps/ui-docs/.storybook/preview.js
+++ apps/ui-docs/.storybook/preview.js
@@ -1,17 +1,17 @@
-import _ from "../node_modules/@hoony-design-system/ui/dist/styles/design-system.scss";
+import _ from '../node_modules/@hoony-design-system/ui/dist/styles/design-system.scss'
 
-console.log("_", _);
+console.log('_', _)
 
 /** @type { import('@storybook/react').Preview } */
 const preview = {
-	parameters: {
-		controls: {
-			matchers: {
-				color: /(background|color)$/i,
-				date: /Date$/i,
-			},
-		},
-	},
-};
+    parameters: {
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/i,
+            },
+        },
+    },
+}
 
-export default preview;
+export default preview
